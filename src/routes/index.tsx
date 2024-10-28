972e88818358db2ab5d137f28fd5bd91d7e35fbe
Johannes Rackles <johannes.rackles@interone.de>
2024-10-28 11:10:02 +0100

    add skeleton

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,20 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import ImgSkeleton from "../components/skeleton.jpg";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
+    <div class="flex min-h-svh">
+      <img
+        src={ImgSkeleton}
+        width={600}
+        height={600}
+        class="m-auto h-full max-w-[600px]"
+      />
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+  title: "It never gets better",
 };
