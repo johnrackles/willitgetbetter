8e7b243f08a857a8cd7a2006e226f92f4818c91a
Johannes Rackles <johannes.rackles@interone.de>
2025-02-20 10:26:36 +0100

    add orb pondering

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,114 +1,26 @@
-import { component$, useVisibleTask$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-import { isServer } from "@builder.io/qwik/build";
-import type { ConfettiOptions } from "@tsparticles/confetti";
-import { confetti } from "@tsparticles/confetti";
-import ImgSkeleton from "../components/skeleton.jpg";
-
-const fireConfetti = () => {
-  const count = 200;
-  const defaults = {
-    origin: { y: 0.7 },
-  };
-
-  function fire(particleRatio: number, opts: ConfettiOptions) {
-    confetti(
-      Object.assign({}, defaults, opts, {
-        particleCount: Math.floor(count * particleRatio),
-      }),
-    );
-  }
-
-  fire(0.25, {
-    spread: 26,
-    startVelocity: 55,
-  });
-
-  fire(0.2, {
-    spread: 60,
-  });
-
-  fire(0.35, {
-    spread: 100,
-    decay: 0.91,
-    scalar: 0.8,
-  });
-
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 25,
-    decay: 0.92,
-    scalar: 1.2,
-  });
-
-  fire(0.1, {
-    spread: 120,
-    startVelocity: 45,
-  });
-};
+import { component$ } from "@builder.io/qwik";
+import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import orb from "../components/ponder-orb.jpg";
 
 export default component$(() => {
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(async () => {
-    const end = Date.now() + 5 * 1000;
-    if (!isServer) {
-      (function frame() {
-        confetti({
-          particleCount: 2,
-          angle: 60,
-          spread: 55,
-          origin: { x: 0 },
-        });
-
-        confetti({
-          particleCount: 2,
-          angle: 120,
-          spread: 55,
-          origin: { x: 1 },
-        });
-
-        if (Date.now() < end) {
-          requestAnimationFrame(frame);
-        }
-      })();
-    }
-  });
-
   return (
-    <div
-      class="flex min-h-svh bg-black"
-      style={{ backgroundImage: `url(${ImgSkeleton})` }}
-    >
-      <div
-        class="flip-container m-auto h-full max-w-full md:max-w-[598px]"
-        style={{ perspective: "1000px" }}
-        onClick$={() => {
-          fireConfetti();
-        }}
+    <div class="flex min-h-svh flex-col justify-center space-y-8 bg-black text-white">
+      <h1 class="text-center text-4xl lg:text-6xl">Will it get better?</h1>
+      <img
+        src={orb}
+        alt="Ponder the orb"
+        width={1920}
+        height={1080}
+        class="aspect-auto h-auto w-full"
+      />
+      <Link
+        href="/never"
+        class="block place-self-center rounded-2xl border-2 border-white p-4 text-xl font-bold transition-colors hover:bg-blue-400 lg:text-2xl"
       >
-        <div class="flipper cursor-pointer">
-          <div class="front max-w-full p-4">
-            <img
-              src={ImgSkeleton}
-              class="max-w-full"
-              width={598}
-              height={588}
-            />
-          </div>
-          <div class="back max-w-full p-4">
-            <img
-              src={ImgSkeleton}
-              class="max-w-full"
-              width={598}
-              height={588}
-            />
-          </div>
-        </div>
-      </div>
+        Ponder the orb
+      </Link>
     </div>
   );
 });
 
-export const head: DocumentHead = {
-  title: "It never gets better",
-};
+export const head: DocumentHead = { title: "Will it get better?" };
---
+++ src/routes/never/index.tsx
@@ -0,0 +1,88 @@
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { isServer } from "@builder.io/qwik/build";
+import type { ConfettiOptions } from "@tsparticles/confetti";
+import { confetti } from "@tsparticles/confetti";
+import ImgSkeleton from "../../components/skeleton.jpg";
+
+const fireConfetti = () => {
+  const count = 200;
+  const defaults = { origin: { y: 0.7 } };
+
+  function fire(particleRatio: number, opts: ConfettiOptions) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      }),
+    );
+  }
+
+  fire(0.25, { spread: 26, startVelocity: 55 });
+
+  fire(0.2, { spread: 60 });
+
+  fire(0.35, { spread: 100, decay: 0.91, scalar: 0.8 });
+
+  fire(0.1, { spread: 120, startVelocity: 25, decay: 0.92, scalar: 1.2 });
+
+  fire(0.1, { spread: 120, startVelocity: 45 });
+};
+
+export default component$(() => {
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(async () => {
+    const end = Date.now() + 5 * 1000;
+    if (!isServer) {
+      (function frame() {
+        confetti({ particleCount: 2, angle: 60, spread: 55, origin: { x: 0 } });
+
+        confetti({
+          particleCount: 2,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1 },
+        });
+
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      })();
+    }
+  });
+
+  return (
+    <div
+      class="flex min-h-svh bg-black"
+      style={{ backgroundImage: `url(${ImgSkeleton})` }}
+    >
+      <div
+        class="flip-container m-auto h-full max-w-full md:max-w-[598px]"
+        style={{ perspective: "1000px" }}
+        onClick$={() => {
+          fireConfetti();
+        }}
+      >
+        <div class="flipper cursor-pointer">
+          <div class="front max-w-full p-4">
+            <img
+              src={ImgSkeleton}
+              class="max-w-full"
+              width={598}
+              height={588}
+            />
+          </div>
+          <div class="back max-w-full p-4">
+            <img
+              src={ImgSkeleton}
+              class="max-w-full"
+              width={598}
+              height={588}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export const head: DocumentHead = { title: "It never gets better" };
